2e1f6a2e0408a31a9eb3ae6a7d147772a2669f8a
suhyeoonn <suhyeon1740@gmail.com>
2024-02-23 12:06:02 +0900

    🎨 카테고리 API 연동

--- src/routes/add/+page.svelte
+++ src/routes/add/+page.svelte
@@ -15,30 +15,47 @@
 
 		location.href = '/';
 	};
+
+	const fetchCategoryList = async () => {
+		const { data } = await axios.get('/api/categories');
+		return data.data;
+	};
+
+	const promise = fetchCategoryList();
 </script>
 
-<form class="h-screen space-y-5" on:submit={onSubmit}>
-	<div>
-		<h2 class="text-2xl">Add Clothes</h2>
-		<p>Enter your clothes information to add to your closet.</p>
-	</div>
-	<div class="input-group">
-		<label for="category">Category</label>
-		<select name="category" id="category" class="select select-bordered" bind:value={data.category}>
-			<option value="1">상의</option>
-			<option value="2">하의</option>
-		</select>
-	</div>
-	<div class="input-group">
-		<label for="name">Name</label>
-		<input type="text" name="name" class="input input-bordered" bind:value={data.itemName} />
-	</div>
-	<div class="input-group">
-		<label for="url">URL</label>
-		<input type="text" name="url" id="url" class="input input-bordered" bind:value={data.image} />
-	</div>
-	<button class="btn btn-primary">Submit</button>
-</form>
+{#await promise}
+	loading...
+{:then categories}
+	<form class="h-screen space-y-5" on:submit={onSubmit}>
+		<div>
+			<h2 class="text-2xl">Add Clothes</h2>
+			<p>Enter your clothes information to add to your closet.</p>
+		</div>
+		<div class="input-group">
+			<label for="category">Category</label>
+			<select
+				name="category"
+				id="category"
+				class="select select-bordered"
+				bind:value={data.category}
+			>
+				{#each categories as category}
+					<option value={category.Id}>{category.Name}</option>
+				{/each}
+			</select>
+		</div>
+		<div class="input-group">
+			<label for="name">Name</label>
+			<input type="text" name="name" class="input input-bordered" bind:value={data.itemName} />
+		</div>
+		<div class="input-group">
+			<label for="url">URL</label>
+			<input type="text" name="url" id="url" class="input input-bordered" bind:value={data.image} />
+		</div>
+		<button class="btn btn-primary">Submit</button>
+	</form>
+{/await}
 
 <style>
 	form {
