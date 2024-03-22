baaa4f5840705d502a427827a57be83c2ab1d380
suhyeoonn <suhyeon1740@gmail.com>
2024-03-22 13:51:00 +0900

    🐛 daisyUI css 설정 복구
    shadcn 설치 시 daisyUI config 설정이 덮어쓰기되어 사라진 현상 해결

--- src/lib/components/Modal.svelte
+++
@@ -1,9 +0,0 @@
-<dialog id="my_modal_2" class="modal">
-  <div class="modal-box">
-    <h3 class="font-bold text-lg">Hello!</h3>
-    <p class="py-4">Press ESC key or click outside to close</p>
-  </div>
-  <form method="dialog" class="modal-backdrop">
-    <button>close</button>
-  </form>
-</dialog>
